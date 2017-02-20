6668caaa0b4e045013f0c899df08639cf5bd3a87
absazamat <absazamat@gmail.com>
2017-02-19 20:37:31 -0800

    inint rule component

---
+++ src/Rule.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Rule extends Component {  
+  /*constructor(props) {
+    super(props);
+    // Operations usually carried out in componentWillMount go here
+  }*/
+
+
+  render() {
+    return (
+      <p>Rule</p>
+    );
+  }
+}
+
+export default Rule;
